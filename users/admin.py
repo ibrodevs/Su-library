a154b79f2d3564f19a8061a29b81bc84c4c4ce9a
Ibro <ibroabdraimov20@gmail.com>
2026-02-22 13:16:21 +0600

    fixed error

--- users/admin.py
+++ users/admin.py
@@ -1,20 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 from unfold.admin import ModelAdmin
 
 
-class UserAdmin(ModelAdmin):
-
+class UserAdmin(ModelAdmin, BaseUserAdmin):
+    
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'group', 'course')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'group', 'course')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
     )
 
-    exclude = ('last_login', 'groups', 'user_permissions', 'date_joined')
-
-    list_display = ('email', 'first_name', 'last_name', 'group', 'course', 'is_active', 'is_staff')
-    list_filter = ('is_active', 'is_staff', 'group', 'course')
-    search_fields = ('email', 'first_name', 'last_name', 'group')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'group', 'course')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
 
 admin.site.register(User, UserAdmin)
